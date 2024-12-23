9c56722a0be182ccf7fbff2bc4f44552e2a82c63
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-12-23 22:00:53 +0530

    Update led-series.ino.ino

--- led-series.ino/led-series.ino.ino
+++ led-series.ino/led-series.ino.ino
@@ -1,13 +1,13 @@
 int delayms = 50;
 int numLEDs = 6;
 void setup() {
-  unsigned char ledPin;                          //ledPin will be set to 1,2,3,4,5,6, 7 and 8.
+  int ledPin;                          //ledPin will be set to 1,2,3,4,5,6, 7 and 8.
   for (ledPin = 1; ledPin <= numLEDs; ledPin++)  //In turn set 1 ~ 8 digital pins to output mode
     pinMode(ledPin, OUTPUT);                     //Set the  ledPin pin to output mode
 }
 
 void loop() {
-  unsigned char ledPin;                          //ledPin will be set to 1,2,3,4,5,6, 7 and 8.
+  int ledPin;                          //ledPin will be set to 1,2,3,4,5,6, 7 and 8.
   for (ledPin = 1; ledPin <= numLEDs; ledPin++)  //Every 200ms on in order LED1 ~ 8
   {
     digitalWrite(ledPin, HIGH);  //led on
