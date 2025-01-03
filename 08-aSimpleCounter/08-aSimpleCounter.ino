eda2aed2b084ba1ef3581a8a0b79b0978b74e357
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2025-01-03 23:38:23 +0530

    Update 08-aSimpleCounter.ino

--- 08-aSimpleCounter/08-aSimpleCounter.ino
+++ 08-aSimpleCounter/08-aSimpleCounter.ino
@@ -1,5 +1,5 @@
-int btn1pin=0;             //Set the digital 0 to button interface 
-int btn2pin=13;            //Set the digital 13 to button interface 
+int btn1pin = 0;  // Set the digital 0 to button interface
+int btn2pin = 13; // Set the digital 13 to button interface
 int a = 1;
 int b = 2;
 int c = 3;
@@ -15,7 +15,7 @@ int d1 = 12;
 
 long n = 0;
 int x = 100;
-int del = 55;  
+int del = 55;
 int i = 0;
 int j = 1;
 int k = 2;
@@ -23,8 +23,8 @@ int l = 3;
 int data;
 void setup()
 {
-  pinMode(btn1pin,INPUT_PULLUP); //Set digital 2 port mode, the INPUT for the input
-  pinMode(btn2pin,INPUT_PULLUP); //Set digital 2 port mode, the INPUT for the input
+  pinMode(btn1pin, INPUT_PULLUP); // Set digital 2 port mode, the INPUT for the input
+  pinMode(btn2pin, INPUT_PULLUP); // Set digital 2 port mode, the INPUT for the input
   pinMode(d1, OUTPUT);
   pinMode(d2, OUTPUT);
   pinMode(d3, OUTPUT);
@@ -38,130 +38,135 @@ void setup()
   pinMode(g, OUTPUT);
   pinMode(p, OUTPUT);
 }
- 
+
 void loop()
 {
-  int d1,d2,d3,d4;
-  if(digitalRead(btn1pin)==LOW)          //Detection button interface to low
-  {   
-      delay(10);                         //Delay 10ms for the elimination of key leading-edge jitter
-      if(digitalRead(btn1pin)==LOW)      //Confirm button is pressed
-      {     
-        if(data>9999)                    
-        { 
-          data=9999;
-        }else{
-          data = data + 1;
-        }
+  int d1, d2, d3, d4;
+  if (digitalRead(btn1pin) == LOW) // Detection button interface to low
+  {
+    delay(10);                       // Delay 10ms for the elimination of key leading-edge jitter
+    if (digitalRead(btn1pin) == LOW) // Confirm button is pressed
+    {
+      if (data > 9999)
+      {
+        data = 9999;
       }
-   }
-   if(digitalRead(btn2pin)==LOW)          //Detection button interface to low
-  {   
-      delay(10);                        //Delay 10ms for the elimination of key leading-edge jitter
-      if(digitalRead(btn2pin)==LOW)      //Confirm button is pressed
-      {     
-        if(data<=0)
-        { 
-          data=0;
-        }else{
-          data = data - 1;
-        }
+      else
+      {
+        data = data + 1;
       }
-   }
-   d1 = data/1000%10;    //The one thousand digital data
-   d2 = data/100%10;     //The one hundred digital data
-   d3 = data/10%10;      //Ten-digit data
-   d4 = data%10;         //data of single digit
-  for(int m=0;m<80;m++){
-  clearLEDs();           //Turn off all LED lights
-  pickDigit(1);          //Selection of a digital display
-  pickNumber(d1);        //Display digital d1
-  delayMicroseconds(200);
- 
-  clearLEDs();           //Turn off all LED lights
-  pickDigit(2);          //Select the first two digital display
-  pickNumber(d2);        //Display digital d2
-  delayMicroseconds(200);
- 
-  clearLEDs();           //Turn off all LED lights
-  pickDigit(3);          //Select the first three digital display
-  //dispDec(3);          //Decimal display
-  pickNumber(d3);        //Display digital d3
-  delayMicroseconds(200);
-  
-  clearLEDs();           //Turn off all LED lights
-  pickDigit(4);          //Select the first four digital display
-  pickNumber(d4);        //Display digital d4
-  delayMicroseconds(200);
+    }
+  }
+  if (digitalRead(btn2pin) == LOW) // Detection button interface to low
+  {
+    delay(10);                       // Delay 10ms for the elimination of key leading-edge jitter
+    if (digitalRead(btn2pin) == LOW) // Confirm button is pressed
+    {
+      if (data <= 0)
+      {
+        data = 0;
+      }
+      else
+      {
+        data = data - 1;
+      }
+    }
+  }
+  d1 = data / 1000 % 10; // The one thousand digital data
+  d2 = data / 100 % 10;  // The one hundred digital data
+  d3 = data / 10 % 10;   // Ten-digit data
+  d4 = data % 10;        // data of single digit
+  for (int m = 0; m < 80; m++)
+  {
+    clearLEDs();    // Turn off all LED lights
+    pickDigit(1);   // Selection of a digital display
+    pickNumber(d1); // Display digital d1
+    delayMicroseconds(200);
+
+    clearLEDs();    // Turn off all LED lights
+    pickDigit(2);   // Select the first two digital display
+    pickNumber(d2); // Display digital d2
+    delayMicroseconds(200);
+
+    clearLEDs();  // Turn off all LED lights
+    pickDigit(3); // Select the first three digital display
+    // dispDec(3);          //Decimal display
+    pickNumber(d3); // Display digital d3
+    delayMicroseconds(200);
+
+    clearLEDs();    // Turn off all LED lights
+    pickDigit(4);   // Select the first four digital display
+    pickNumber(d4); // Display digital d4
+    delayMicroseconds(200);
   }
 }
- 
-void pickDigit(int x)  //Defined pickDigit (x), whose role is to open the port dx
+
+void pickDigit(int x) // Defined pickDigit (x), whose role is to open the port dx
 {
   digitalWrite(d1, HIGH);
   digitalWrite(d2, HIGH);
   digitalWrite(d3, HIGH);
   digitalWrite(d4, HIGH);
-  switch(x)
+  switch (x)
   {
-    case 1: 
-           digitalWrite(d1, LOW); 
-           break;
-    case 2: 
-           digitalWrite(d2, LOW); 
-           break;
-    case 3: 
-           digitalWrite(d3, LOW); 
-           break;
-    default: 
-           digitalWrite(d4, LOW); 
-           break;
+  case 1:
+    digitalWrite(d1, LOW);
+    break;
+  case 2:
+    digitalWrite(d2, LOW);
+    break;
+  case 3:
+    digitalWrite(d3, LOW);
+    break;
+  default:
+    digitalWrite(d4, LOW);
+    break;
   }
 }
- 
-void pickNumber(int x)   //Defined pickNumber (x), whose role is to display digital x
+
+void pickNumber(int x) // Defined pickNumber (x), whose role is to display digital x
 {
-  switch(x)
+  switch (x)
   {
-    case 1: 
-           one(); 
-           break;
-    case 2: 
-           two(); 
-           break;
-    case 3: 
-           three(); 
-           break;
-    case 4: 
-           four(); 
-           break;
-    case 5: 
-           five(); 
-           break;
-    case 6: 
-           six(); 
-           break;
-    case 7: 
-	   seven(); 
-	   break;
-    case 8: 
-	   eight(); 
-	   break;
-    case 9: 
-	   nine(); 
-	   break;
-    default: 
-           zero(); 
-           break;
+  case 1:
+    one();
+    break;
+  case 2:
+    two();
+    break;
+  case 3:
+    three();
+    break;
+  case 4:
+    four();
+    break;
+  case 5:
+    five();
+    break;
+  case 6:
+    six();
+    break;
+  case 7:
+    seven();
+    break;
+  case 8:
+    eight();
+    break;
+  case 9:
+    nine();
+    break;
+  default:
+    zero();
+    break;
   }
 }
-	 
-void dispDec(int x)  //Decimal point setting Open
+
+void dispDec(int x) // Decimal point setting Open
 {
   digitalWrite(p, HIGH);
 }
- 
-void clearLEDs()  //Clear screen
+
+void clearLEDs() // Clear screen
 {
   digitalWrite(a, LOW);
   digitalWrite(b, LOW);
@@ -172,8 +177,8 @@ void clearLEDs()  //Clear screen
   digitalWrite(g, LOW);
   digitalWrite(p, LOW);
 }
- 
-void zero()  //Define those figures 0 cathode pin switch
+
+void zero() // Define those figures 0 cathode pin switch
 {
   digitalWrite(a, HIGH);
   digitalWrite(b, HIGH);
@@ -183,8 +188,8 @@ void zero()  //Define those figures 0 cathode pin switch
   digitalWrite(f, HIGH);
   digitalWrite(g, LOW);
 }
- 
-void one()  //Define those figures 1 cathode pin switch
+
+void one() // Define those figures 1 cathode pin switch
 {
   digitalWrite(a, LOW);
   digitalWrite(b, HIGH);
@@ -194,8 +199,8 @@ void one()  //Define those figures 1 cathode pin switch
   digitalWrite(f, LOW);
   digitalWrite(g, LOW);
 }
- 
-void two()  //Define those figures 2 cathode pin switch
+
+void two() // Define those figures 2 cathode pin switch
 {
   digitalWrite(a, HIGH);
   digitalWrite(b, HIGH);
@@ -205,8 +210,8 @@ void two()  //Define those figures 2 cathode pin switch
   digitalWrite(f, LOW);
   digitalWrite(g, HIGH);
 }
- 
-void three()  //Define those figures 3 cathode pin switch
+
+void three() // Define those figures 3 cathode pin switch
 {
   digitalWrite(a, HIGH);
   digitalWrite(b, HIGH);
@@ -216,8 +221,8 @@ void three()  //Define those figures 3 cathode pin switch
   digitalWrite(f, LOW);
   digitalWrite(g, HIGH);
 }
- 
-void four()  //Define those figures 4 cathode pin switch
+
+void four() // Define those figures 4 cathode pin switch
 {
   digitalWrite(a, LOW);
   digitalWrite(b, HIGH);
@@ -227,8 +232,8 @@ void four()  //Define those figures 4 cathode pin switch
   digitalWrite(f, HIGH);
   digitalWrite(g, HIGH);
 }
- 
-void five()  //Define those figures 5 cathode pin switch
+
+void five() // Define those figures 5 cathode pin switch
 {
   digitalWrite(a, HIGH);
   digitalWrite(b, LOW);
@@ -238,8 +243,8 @@ void five()  //Define those figures 5 cathode pin switch
   digitalWrite(f, HIGH);
   digitalWrite(g, HIGH);
 }
- 
-void six()  //Define those figures 6 cathode pin switch
+
+void six() // Define those figures 6 cathode pin switch
 {
   digitalWrite(a, HIGH);
   digitalWrite(b, LOW);
@@ -249,8 +254,8 @@ void six()  //Define those figures 6 cathode pin switch
   digitalWrite(f, HIGH);
   digitalWrite(g, HIGH);
 }
- 
-void seven()  //Define those figures 7 cathode pin switch
+
+void seven() // Define those figures 7 cathode pin switch
 {
   digitalWrite(a, HIGH);
   digitalWrite(b, HIGH);
@@ -260,8 +265,8 @@ void seven()  //Define those figures 7 cathode pin switch
   digitalWrite(f, LOW);
   digitalWrite(g, LOW);
 }
- 
-void eight()  //Define those figures 8 cathode pin switch
+
+void eight() // Define those figures 8 cathode pin switch
 {
   digitalWrite(a, HIGH);
   digitalWrite(b, HIGH);
@@ -271,8 +276,8 @@ void eight()  //Define those figures 8 cathode pin switch
   digitalWrite(f, HIGH);
   digitalWrite(g, HIGH);
 }
- 
-void nine()  //Define those figures 9 cathode pin switch
+
+void nine() // Define those figures 9 cathode pin switch
 {
   digitalWrite(a, HIGH);
   digitalWrite(b, HIGH);
@@ -282,5 +287,3 @@ void nine()  //Define those figures 9 cathode pin switch
   digitalWrite(f, HIGH);
   digitalWrite(g, HIGH);
 }
-
-
